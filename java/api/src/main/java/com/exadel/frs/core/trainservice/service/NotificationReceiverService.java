9cad0dca7a83a7643ff92f48d6779ab96077f05f
Rafael Baptista <rafaelbaptista@ua.pt>
2022-06-24 11:12:56 +0100

    Added some prints

--- java/api/src/main/java/com/exadel/frs/core/trainservice/service/NotificationReceiverService.java
+++ java/api/src/main/java/com/exadel/frs/core/trainservice/service/NotificationReceiverService.java
@@ -15,6 +15,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -56,7 +58,9 @@ public class NotificationReceiverService {
             }
         };
         try {
-            connection = pgNotificationDatasource.getConnection().unwrap(PGConnection.class);
+            Connection get_connection = pgNotificationDatasource.getConnection();
+            System.out.println(get_connection);
+            connection = get_connection.unwrap(PGConnection.class);
             System.out.println(connection);
             Statement statement = connection.createStatement();
             statement.executeUpdate("LISTEN face_collection_update_msg");
